9897f9b9fe4333061d536916533792eb2907a11e
Dmitry Chaplinsky <chaplinsky.dmitry@gmail.com>
2013-06-10 01:21:34 +0300

    Trying to beat file upload

--- webapp.py
+++ webapp.py
@@ -1,11 +1,11 @@
-import os
-import logging
-from StringIO import StringIO
+# import os
+# import logging
+# from StringIO import StringIO
 
-from flask import Flask, request, redirect #, url_for
+from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
 
-from reader import MusicXMLReader
+# from reader import MusicXMLReader
 
 UPLOAD_FOLDER = 'tmp'
 ALLOWED_EXTENSIONS = set(['xml'])
@@ -22,32 +22,34 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    print "hello"
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
-            filename = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
-            file.save(filename)
+            return file.getvalue()
+            # filename = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+            # file.save(filename)
 
-            buff = StringIO()
-            rootLogger = logging.getLogger()
-            logHandler = logging.StreamHandler(buff)
-            rootLogger.addHandler(logHandler)
-            rootLogger.setLevel(logging.INFO)
+            # buff = StringIO()
+            # rootLogger = logging.getLogger()
+            # logHandler = logging.StreamHandler(buff)
+            # rootLogger.addHandler(logHandler)
+            # rootLogger.setLevel(logging.INFO)
 
-            MusicXMLReader(filename)
+            # MusicXMLReader(filename)
 
-            rootLogger.removeHandler(logHandler)
-            logHandler.flush()
-            buff.flush()
-            # print(buff.read())
-            return "<pre>%s</pre>" % buff.getvalue()
-        else:
-            return '''
-            <!doctype html>
-            <title>Upload new File</title>
-            <h1>Upload new File</h1>
-            <form action="" method=post enctype=multipart/form-data>
-              <p><input type=file name=file>
-                 <input type=submit value=Upload>
-            </form>
-            '''
+            # rootLogger.removeHandler(logHandler)
+            # logHandler.flush()
+            # buff.flush()
+            # # print(buff.read())
+            # return "<pre>%s</pre>" % buff.getvalue()
+    else:
+        return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form action="" method=post enctype=multipart/form-data>
+          <p><input type=file name=file>
+             <input type=submit value=Upload>
+        </form>
+        '''
